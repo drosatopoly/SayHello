cf11f28baf9e0ac076f031dafcbaa86ef472b772
Ethan Kim <ekim23@students.polytechnic.org>
2023-01-10 14:10:44 -0800

    readded my deleted hello

--- SayHello.java
+++ SayHello.java
@@ -15,7 +15,7 @@ public class SayHello{
         System.out.println("Hello from Phoebe Salvati");
         System.out.println("Hello from Kate Lim");
         System.out.println("Hello from James Miller");
-
+        System.out.println("Ethan Kim");
 
 
 
