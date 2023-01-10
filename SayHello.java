b7ca4b1d991f7fbc9ec691c729b1ce545340f283
swagsosa <gcho24@students.polytechnic.org>
2023-01-10 12:16:58 -0800

    SayHello

--- SayHello.java
+++ SayHello.java
@@ -5,7 +5,6 @@ public class SayHello{
     public static void main(String[] args) {
         System.out.println("Let's see who has worked on this project!");
         //Add a greeting statement from yourself!
-        System.out.println("Hello from Mr. Rosato!");
-        System.out.println("hello from esteban");
+        System.out.println("Hello from Gavin Cho");
     }
 }
