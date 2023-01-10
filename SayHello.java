007f793f6c26e9feb1d04150164ed79c9be8c7d9
mychaelmunder <122400385+mychaelmunder@users.noreply.github.com>
2023-01-10 12:22:52 -0800

    Update SayHello.java
    Hello from Michael

--- SayHello.java
+++ SayHello.java
@@ -8,5 +8,6 @@ public class SayHello{
         System.out.println("Hello from Gavin Cho");
         System.out.println("Hello from Asher Singla");
         System.out.println("Hello from Dario Soatto");
+        System.out.println("Hello from Michael Munder");
     }
 }
