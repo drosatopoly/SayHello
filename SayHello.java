9f981df867bf3a6fb28e00a53912cabbb45c8a25
Dominic Rosato <drosato@polytechnic.org>
2023-01-10 09:13:54 -0800

    Included SayHello.java

---
+++ SayHello.java
@@ -0,0 +1,10 @@
+/**
+ * This program is made to show how collaboration can work on a single GitHub project!
+ */
+public class SayHello{
+    public static void main(String[] args) {
+        System.out.println("Let's see who has worked on this project!");
+        //Add a greeting statement from yourself!
+        System.out.println("Hello from Mr. Rosato!");
+    }
+}
